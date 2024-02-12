508958fd7bf7be11934d46254bb986c72df2d517
KramPiotr <piotrek@krambit.pl>
2024-02-12 03:13:46 +0100

    Add storage and retrieval of NFTs that would store the bApp data

--- deploy.zsh
+++ deploy.zsh
@@ -3,6 +3,7 @@
 dfx deploy template_backend
 dfx deploy template_frontend
 dfx deploy bapp_marketplace
+dfx deploy wall3
 dfx deps deploy
 dfx deps init internet_identity --argument null
 ./deploy_icp_ledger_canister.zsh
